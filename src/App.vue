e798bfd5f380ebc46393e6ea26bc7231bc366098
5OO <git.hub@myrk.eu>
2023-10-21 14:40:02 +0300

    refactored map related code into Vue component MushroomMap.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <template>
   <div id="app">
-    <l-map style="height: 100vh; width: 100%;" :zoom="15" :center="[59.396153, 24.657925]">
-      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
-      <l-marker v-for="marker in markers" :key="marker.id" :lat-lng="marker.latLng"></l-marker>
-    </l-map>
+    <MushroomMap/>
   </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet';
-import 'leaflet/dist/leaflet.css';
+import MushroomMap from "@/components/MushroomMap.vue";
 
 export default {
+  name: 'App',
   components: {
-    LMap,
-    LTileLayer,
-    LMarker
-    },
-
-  data() {
-    return {
-      markers: [
-        { id: 1, latLng: [59.396153, 24.657925] },
-        { id: 2, latLng: [59.397153, 24.658925] },
-        { id: 3, latLng: [59.398153, 24.659925] },
-        { id: 4, latLng: [59.396653, 24.656925] },
-        { id: 5, latLng: [59.398153, 24.655925] },
-        { id: 6, latLng: [59.398153, 24.652925] },
-      ]
-    }
+    MushroomMap
   }
+
 }
 </script>
---
+++ src/components/MushroomMap.vue
@@ -0,0 +1,70 @@
+<template>
+  <l-map
+      :center="center"
+      :zoom="zoom"
+      class="map"
+      ref="map"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+  >
+    <l-tile-layer
+        :url="url"
+    ></l-tile-layer>
+
+    <l-marker
+        v-for="marker in markers"
+        :key="marker.id"
+        :lat-lng="marker.latLng"
+    ></l-marker>
+  </l-map>
+
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+  name: "MushroomMap",
+  components:{
+    LMarker,
+    LMap,
+    LTileLayer
+  },
+  data() {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      center: [59.396153, 24.657925],
+      zoom: 15,
+      markers: [
+            { id: 1, latLng: [59.396153, 24.657925] },
+            { id: 2, latLng: [59.397153, 24.658925] },
+            { id: 3, latLng: [59.398153, 24.659925] },
+            { id: 4, latLng: [59.396653, 24.656925] },
+            { id: 5, latLng: [59.398153, 24.655925] },
+            { id: 6, latLng: [59.398153, 24.652925] },
+          ]
+    }
+  },
+  methods:{
+      zoomUpdated(zoom) {
+      this.zoom = zoom;
+      console.log(this.markers)
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+  }
+}
+</script>
+
+
+<style>
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+
+</style>
