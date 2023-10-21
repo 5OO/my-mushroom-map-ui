c0fbf5db9df6fb6a2df8165f902b479f7137a83e
5OO <git.hub@myrk.eu>
2023-10-21 13:00:48 +0300

    added marker array for test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <l-map style="height: 100vh; width: 100%;" :zoom="13" :center="[59.396153, 24.657925]">
+    <l-map style="height: 100vh; width: 100%;" :zoom="15" :center="[59.396153, 24.657925]">
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
-      <l-marker :lat-lng="[59.396153, 24.657925]"></l-marker>
+      <l-marker v-for="marker in markers" :key="marker.id" :lat-lng="marker.latLng"></l-marker>
     </l-map>
   </div>
 </template>
@@ -16,6 +16,19 @@ export default {
     LMap,
     LTileLayer,
     LMarker
+    },
+
+  data() {
+    return {
+      markers: [
+        { id: 1, latLng: [59.396153, 24.657925] },
+        { id: 2, latLng: [59.397153, 24.658925] },
+        { id: 3, latLng: [59.398153, 24.659925] },
+        { id: 4, latLng: [59.396653, 24.656925] },
+        { id: 5, latLng: [59.398153, 24.655925] },
+        { id: 6, latLng: [59.398153, 24.652925] },
+      ]
     }
+  }
 }
 </script>
